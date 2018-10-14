ee77cc808d541ce18f118ab6aaa7b442258d9c55
alexHooper <alexmohack@gmail.com>
2018-10-14 16:49:07 +0530

    add admin user permission

--- quickstart/views.py
+++ quickstart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from quickstart.serializers import UserSerializer, GroupSerializer
 
@@ -10,7 +10,7 @@ class UserViewSet(viewsets.ModelViewSet):
 	"""
 	API endpoint that allows users to be viewed or edited
 	"""
-	permission_classes = (IsAuthenticated,)
+	permission_classes = (IsAdminUser,)
 	queryset = User.objects.all().order_by('-date_joined')
 	serializer_class = UserSerializer
 
@@ -19,6 +19,6 @@ class GroupViewSet(viewsets.ModelViewSet):
 	"""
 	API endpoint that allows groups to be viewed or edited
 	"""
-	permission_classes = (IsAuthenticated,)
+	permission_classes = (IsAdminUser,)
 	queryset = Group.objects.all()
 	serializer_class = GroupSerializer
