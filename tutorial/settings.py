4a95ea6a06b898f4a08139a6ec3271c199e435b6
alexHooper <alexmohack@gmail.com>
2018-10-14 16:47:24 +0530

    add permission classes in settings

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -111,7 +111,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # REST FRAMEWORK
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
 
 
