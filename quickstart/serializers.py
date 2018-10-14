74b64be9741c0fd11930ab5e1fb58a21b0480c6b
alexHooper <alexmohack@gmail.com>
2018-10-14 16:18:16 +0530

    serializer for user and group model

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,1 +1,14 @@
+from django.contrib.auth.models import User, Group
 
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperLinkedModelSerializer):
+	class Meta:
+		model = User
+		fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperLinkedModelSerializer):
+	class Meta:
+		model = Group
+		fields = ('url', 'name')
