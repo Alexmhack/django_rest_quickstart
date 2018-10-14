23ecd19ab2194c8cf944c1cb5cd35b22034f5c8c
alexHooper <alexmohack@gmail.com>
2018-10-14 16:47:35 +0530

    add is authenticated permission in rest view

--- quickstart/views.py
+++ quickstart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from quickstart.serializers import UserSerializer, GroupSerializer
 
@@ -9,6 +10,7 @@ class UserViewSet(viewsets.ModelViewSet):
 	"""
 	API endpoint that allows users to be viewed or edited
 	"""
+	permission_classes = (IsAuthenticated,)
 	queryset = User.objects.all().order_by('-date_joined')
 	serializer_class = UserSerializer
 
@@ -17,5 +19,6 @@ class GroupViewSet(viewsets.ModelViewSet):
 	"""
 	API endpoint that allows groups to be viewed or edited
 	"""
+	permission_classes = (IsAuthenticated,)
 	queryset = Group.objects.all()
 	serializer_class = GroupSerializer
